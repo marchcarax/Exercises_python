b24992e851c3f5a0ea859d041e41a4f655813309
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-27 13:20:15 +0200

    exercise

--- square_sum.py
+++ square_sum.py
@@ -30,11 +30,29 @@ def squaresumodds(a: int):
     arr = np.sum(arr)
     return arr
 
+'''
+The sum of the squares of the first ten natural numbers is,
+
+The square of the sum of the first ten natural numbers is,
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+'''
+def sumsquaredif(a: int):
+    sum_squares = squaresum(a)
+    arr = np.arange(a)
+    arr = np.sum(arr)*np.sum(arr)
+    return (arr - sum_squares)
+
 
 def main():
-    a = 12
+    a = 101
+    print('a vector: ', np.arange(a))
     print('sum of squares of a: ', squaresum(a))
     print('sum of squares of odds of a: ', squaresumodds(a))
+    print('difference between sum of squares: ', sumsquaredif(a))
 
 if __name__=='__main__':
     main()
